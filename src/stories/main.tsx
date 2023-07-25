f146180fa001f1e3f08482dde707b576d6d63aa7
Home <home@Homes-MacBook-Pro.local>
2023-07-25 23:59:32 +0300

    Final setup

---
+++ src/stories/main.tsx
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from '@storybook/react';
+
+const config: StorybookConfig = {
+  // Required
+  framework: '@storybook/your-framework',
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  // Optional
+  addons: ['@storybook/addon-essentials'],
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+};
+
+export default config;
